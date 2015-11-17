ada434396d0234312d65d3143e03c91f59c5f102
Brianna Karle <brianna.risard@gmail.com>
2015-11-16 16:59:41 -0800

    This is my second change to test.php
    Change version number, added h2, changed one <p>

--- test.php
+++ test.php
@@ -3,8 +3,8 @@
 </head>
 <body>
 <h1>Imagine that this is a cool webpage</h1>
-<p>Version 2</p>
-<p>Clever Content goes here</p>
+<h2>Version 3</h2>
+<p>Different clever Content goes here</p>
 <p>Clever Content goes here</p>
 <p>Clever Content goes here</p>
 </body>
