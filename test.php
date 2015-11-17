0663f2131a4a53db6455fd8cd417110856e63261
Brianna Karle <brianna.risard@gmail.com>
2015-11-16 16:52:21 -0800

    Initial test page made
    Playing around in itc240

---
+++ test.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+</head>
+<body>
+<h1>Imagine that this is a cool webpage</h1>
+<p>Version 2</p>
+<p>Clever Content goes here</p>
+<p>Clever Content goes here</p>
+<p>Clever Content goes here</p>
+</body>
+
+
+
+
+
+</html>
